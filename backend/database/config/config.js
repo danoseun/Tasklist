e184629100c9058a655b6304717bbca6fc56f457
Chris Scott <scott.chriss@gmail.com>
2020-08-30 16:03:54 -0400

    generate task model and migration field with sequelize-cli create database

---
+++ backend/database/config/config.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+module.exports = {
+  development: {
+    url: process.env.DEV_DATABASE_URL,
+    dialect: 'mysql',
+  },
+  test: {
+    url: process.env.TEST_DATABASE_URL,
+    dialect: 'mysql',
+  },
+  production: {
+    url: process.env.DATABASE_URL,
+    dialect: 'mysql',
+  },
+}
+
