6410be6519bd4f12af8e52d75332ddcd056f21b9
Chris Scott <scott.chriss@gmail.com>
2020-08-30 13:50:07 -0400

    install project dependencies bootstrap project add babel for transpiling

---
+++ backend/server.js
@@ -0,0 +1,11 @@
+import express from 'express';
+
+const app = express();
+
+const port = process.env.PORT || 8000;
+
+app.listen(port, () => {
+    console.log(`server is running on port ${port}`);
+});
+
+export default app;
